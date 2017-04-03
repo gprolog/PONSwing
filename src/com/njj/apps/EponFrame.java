6274bc74a16c4d17258ff5943d4ed6b302a238e9
njj08 <njj08@DESKTOP-1RM960H>
2017-04-03 22:19:34 +0800

    zenbook3_201704032220

---
+++ src/com/njj/apps/EponFrame.java
@@ -0,0 +1,38 @@
+package com.njj.apps;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class EponFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6835999048166516811L;
+
+//	private EponFrame eFrame;
+	
+	// Setting the width and height of frame
+//	frame.setSize(800, 600);
+//	frame.setLocationRelativeTo(null);
+//	frame.setResizable(false);
+//	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//	JPanel panel = new JPanel();
+//	// 添加面板
+//	frame.add(panel);
+//	/*
+//	 * 调用用户定义的方法并添加组件到面板
+//	 */
+//	placeComponents(panel);
+	public EponFrame(){
+		this.setTitle("贝尔7342->华为5680T PON口割接脚本生成工具");
+		this.setSize(1000,800);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+
+}
---
+++ src/com/njj/apps/GponFrame.java
@@ -0,0 +1,12 @@
+package com.njj.apps;
+
+import javax.swing.JFrame;
+
+public class GponFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3427007581144913433L;
+
+}
